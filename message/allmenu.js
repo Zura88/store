ae29d9e6716b6f9e96a06d4a97b4b03269ffcda7
Zura88 <agungyudisaputro@gmail.com>
2022-03-28 14:54:46 +0700

    store

---
+++ message/allmenu.js
@@ -0,0 +1,117 @@
+*hallo kak @${sender.split('@')[0]}👋 ${ucapanWaktu}*
+notes : Jangan spam ya kak, kasih jeda 5 detik agar bot tidak delay, okeh kak?
+
+*⌜ Grup Menu ⌟* 
+• ${prefix}welcome <enable/disable>
+• ${prefix}antilink <enable/disable>
+• ${prefix}group <enable/disable>
+• ${prefix}simi <enable/disable>
+• ${prefix}kick <@user>
+• ${prefix}kickall
+• ${prefix}hedsot <@user>
+• ${prefix}reportbug
+• ${prefix}request
+• ${prefix}here
+• ${prefix}setdesk
+• ${prefix}setnamegc
+• ${prefix}setppgrup
+• ${prefix}promote
+• ${prefix}demote
+• ${prefix}promoteall
+• ${prefix}demoteall
+• ${prefix}wame
+• ${prefix}notif
+• ${prefix}listadmin
+• ${prefix}listonline
+• ${prefix}linkgrup
+
+*⌜ Game Menu ⌟* 
+• ${prefix}truth
+• ${prefix}dare  
+• ${prefix}slot
+• ${prefix}suit
+• ${prefix}gelud
+• ${prefix}tebakkata
+• ${prefix}tebakkimia
+• ${prefix}tebaklirik
+• ${prefix}tebakbendera
+• ${prefix}tebakgambar
+• ${prefix}tebakanime
+• ${prefix}math
+• ${prefix}siapaaku
+• ${prefix}tictactoe
+• ${prefix}family100
+• ${prefix}caklontong
+
+*⌜ Download Menu ⌟* 
+• ${prefix}play <judul lagu>
+• ${prefix}ytmp3 <judul/link>
+• ${prefix}ytmp4 <judul/link>
+• ${prefix}shortlink <link>
+• ${prefix}mediafire <link>
+• ${prefix}playstore <nama apk>
+• ${prefix}appsstore <nama apl>
+• ${prefix}y2mate <link>
+• ${prefix}tiktok <judul>
+• ${prefix}tiktokmusic <link>
+• ${prefix}tiktoknowm <link>
+• ${prefix}tiktokwm <link>
+• ${prefix}igvideo <link>
+• ${prefix}igfoto <link>
+• ${prefix}ktp <isi dengan lengkap>
+• ${prefix}pinterest <text>
+• ${prefix}lirik <text>
+• ${prefix}herolist <text>
+• ${prefix}herodetail <nama hero>
+
+*⌜ Sticker Menu ⌟* 
+• ${prefix}sticker <reply image>
+• ${prefix}ttp <text>
+• ${prefix}attp <text>
+• ${prefix}attp2 <text>
+• ${prefix}attp3 <text>
+• ${prefix}attp4 <text>
+• ${prefix}attp5 <text>
+• ${prefix}amongus <text>
+• ${prefix}patrick <text>
+• ${prefix}toimg <reply sticker>
+
+*⌜ Kode Menu ⌟*
+• ${prefix}tts <text>
+• ${prefix}kodenegara <text>
+• ${prefix}kodebahasa <text>
+
+*⌜ Tools Menu ⌟*
+• ${prefix}ssweb
+• ${prefix}ssurl
+• ${prefix}sslink
+• ${prefix}encimg
+• ${prefix}encfoto
+
+*⌜ Owner Menu ⌟* 
+• ${prefix}broadcast <text>
+• ${prefix}broadcast2 <text>
+• ${prefix}broadcastvideo <text>
+• ${prefix}bcvideo <text>
+• ${prefix}bc <text>
+• ${prefix}bc2 <text>
+• ${prefix}bc3 <text>
+• ${prefix}bc4 <text>
+• ${prefix}bc5 <text>
+• ${prefix}delete <reply message>
+• ${prefix}ban <target>
+• ${prefix}unban <target>
+• ${prefix}block <user>
+• ${prefix}unblock <user>
+• ${prefix}settppbot <image>
+• ${prefix}leaveall (keluar semua grup)
+• ${prefix}join <link grup>
+• ${prefix}join2 <link grup>
+• ${prefix}public
+• ${prefix}self
+• ${prefix}clearall
+• ${prefix}restart
+• ${prefix}addcmd
+• ${prefix}delcmd
+• ${prefix}listcmd
+• ${prefix}exif
